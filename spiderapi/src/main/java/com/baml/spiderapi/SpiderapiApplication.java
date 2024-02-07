b7aba81f31c6d5dd4ec4c7690b085417cb07f775
mahir <mahireddy45@gmail.com>
2024-02-07 08:53:23 +0530

    First commit

---
+++ spiderapi/src/main/java/com/baml/spiderapi/SpiderapiApplication.java
@@ -0,0 +1,13 @@
+package com.baml.spiderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiderapiApplication.class, args);
+	}
+
+}
---
+++ spiderapi/src/test/java/com/baml/spiderapi/SpiderapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baml.spiderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiderapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
